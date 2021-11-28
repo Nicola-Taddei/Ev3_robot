89a6253c88b1c0a54a6b1b7b8c68cbda2e96f6e9
Nicola <nicola.taddei@mail.polimi.it>
2021-11-28 11:13:07 +0100

    nov 27 changes

--- src/robot.py
+++ src/robot.py
@@ -1,17 +1,37 @@
 from ev3dev.ev3 import *
+from time import sleep
+
+COLORS = ['unknown', 'black', 'blue', 'green', 'yellow', 'red', 'white', 'brown']
 
 class Robot():
-    def __init__(self, portSx, portDx):
-        self.motorSx, self.motorDx = MediumMotor(portSx), MediumMotor(portDx)
-    
+    def __init__(self, mSx, mDx, cSx, cDx):
+        self.motorSx, self.motorDx, self.colorSx, self.colorDx = MediumMotor(mSx), MediumMotor(mDx), ColorSensor(cSx), ColorSensor(cDx)
+        self.colorSx.mode, self.colorDx.mode = 'COL-COLOR', 'COL-COLOR'
+
     def move(self, speedSx, speedDx):
         self.motorSx.run_forever(speed_sp=speedSx)
-        self.motorDx.run_forever(spped_sp=-speedDx)
+        self.motorDx.run_forever(speed_sp=-speedDx)
 
     def stop(self):
         self.motorSx.stop(stop_action='hold')
         self.motorDx.stop(stop_action='hold')
 
+    def readColor(self):
+        return (COLORS[self.colorSx.value()], COLORS[self.colorDx.value()])
+
+    def drive(self, speedSx, speedDx, correction):
+        while True:
+            tmpSx, tmpDx = speedSx, speedDx
+            color = self.readColor()
+            if color[0] == 'black':
+                tmpDx = speedDx + correction
+            if color[1] == 'black':
+                tmpSx = speedSx + correction
+            if color == ('black', 'black'):
+                self.stop()
+                sleep(5)
+            self.move(tmpSx, tmpDx)
+            sleep(0.2)
 
 if __name__=='main':
-    r = Robot('outA', 'outB')
+    r = Robot('outA', 'outB', 'in1', 'in2')
--- src/test.py
+++ src/test.py
@@ -1,24 +1,27 @@
-#! /usr/bin/env/python3
-
+#! /usr/bin/python3
 from ev3dev.ev3 import *
 from time import sleep
 from robot import Robot
 
-STD_SPEED = 500
+STD_DIR_SPEED=300
+STD_ROT_SPEED=200
 STD_DELAY = 3
 
-r = Robot('outA', 'outB')
+r = Robot('outB', 'outA', 'in1', 'in2') # B->sinistro e A->destro
 
 sleep(5)
 
+speed=STD_DIR_SPEED
+
+r.drive(300, 300, 150)
+
+'''
 while True:
-    r.move(STD_SPEED, STD_SPEED)
-    sleep(STD_DELAY)
-    r.stop()
-    sleep(STD_DELAY)
-    r.move(STD_SPEED, -STD_SPEED)
-    sleep(STD_DELAY)
-    r.stop()
-    sleep(STD_DELAY)
-    r.move(-STD_SPEED, STD_SPEED)
-    sleep(STD_DELAY)
+    sleep(0.1)
+    color = r.readColor()
+    if color==('black', 'black'):
+        speed=-speed
+        r.move(speed, speed)
+        sleep(1)
+    r.move(speed, speed)
+'''
