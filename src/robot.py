f74a4730fc7e34650a761a14cc8c3ff31f6f9922
Nicola <nicola.taddei@mail.polimi.it>
2021-11-28 16:34:30 +0100

    Introduce Controller class

--- src/robot.py
+++ src/robot.py
@@ -3,8 +3,14 @@ from time import sleep
 
 COLORS = ['unknown', 'black', 'blue', 'green', 'yellow', 'red', 'white', 'brown']
 
+k_P = 15
+k_I = 5
+
 class Robot():
-    def __init__(self, mSx, mDx, cSx, cDx):
+    def __init__(self, mSx, mDx, cSx, cDx, controller, f, SPEED):
+        self.SPEED = SPEED
+        self.controller = controller   #allows to use a generic kind of controller, instead of forcing the use of Controller class
+        self.f = f
         self.motorSx, self.motorDx, self.colorSx, self.colorDx = MediumMotor(mSx), MediumMotor(mDx), ColorSensor(cSx), ColorSensor(cDx)
         self.colorSx.mode, self.colorDx.mode = 'COL-COLOR', 'COL-COLOR'
 
@@ -19,19 +25,41 @@ class Robot():
     def readColor(self):
         return (COLORS[self.colorSx.value()], COLORS[self.colorDx.value()])
 
-    def drive(self, speedSx, speedDx, correction):
+    def drive(self):
+        e = 0
         while True:
-            tmpSx, tmpDx = speedSx, speedDx
             color = self.readColor()
             if color[0] == 'black':
-                tmpDx = speedDx + correction
+                if e > 0:
+                    e = 0
+                e -= 1
             if color[1] == 'black':
-                tmpSx = speedSx + correction
+                if e < 0:
+                    e = 0
+                e += 1
             if color == ('black', 'black'):
                 self.stop()
                 sleep(5)
-            self.move(tmpSx, tmpDx)
-            sleep(0.2)
+            corr = self.controller.getCorrection(e, self.SPEED)
+            self.move(self.SPEED - corr, self.SPEED + corr)
+            sleep(1/self.f)
+
+
+class Controller():
+    def __init__(self, k_P, k_I):
+        self.k_P, self.k_I = k_P, k_I
+
+    def sgn(self, x):
+        if x < 0:
+            return -1
+        if x == 0:
+            return 0
+        return 1
+
+    def getCorrection(self, error, SPEED):  #error has integer values: negative if black is detected on the left and positive if it is detected on the right. The absolute value is the number of consecutive identical mesurements 
+        P = -k_P * self.sgn(error) * SPEED
+        I = -k_I * error * SPEED
+        return P + I
 
 if __name__=='main':
-    r = Robot('outA', 'outB', 'in1', 'in2')
+    Robot('outB', 'outA', 'in1', 'in2', Controller(15, 5), 10, 200).drive()
--- src/test.py
+++ src/test.py
@@ -1,27 +1,12 @@
 #! /usr/bin/python3
 from ev3dev.ev3 import *
 from time import sleep
-from robot import Robot
+from robot import Robot, Controller
 
-STD_DIR_SPEED=300
-STD_ROT_SPEED=200
-STD_DELAY = 3
+STD_DIR_SPEED=200
 
-r = Robot('outB', 'outA', 'in1', 'in2') # B->sinistro e A->destro
+r = Robot('outB', 'outA', 'in1', 'in2', Controller(15, 5), 10, STD_DIR_SPEED) # B->sinistro e A->destro
 
 sleep(5)
 
-speed=STD_DIR_SPEED
-
-r.drive(300, 300, 150)
-
-'''
-while True:
-    sleep(0.1)
-    color = r.readColor()
-    if color==('black', 'black'):
-        speed=-speed
-        r.move(speed, speed)
-        sleep(1)
-    r.move(speed, speed)
-'''
+r.drive()
