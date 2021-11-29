d68dbe2651014cbfa75c46f6df29326f07363333
Nicola <nicola.taddei@mail.polimi.it>
2021-11-29 23:08:50 +0100

    Complete line follower

--- src/robot.py
+++ src/robot.py
@@ -3,11 +3,11 @@ from time import sleep
 
 COLORS = ['unknown', 'black', 'blue', 'green', 'yellow', 'red', 'white', 'brown']
 
-k_P = 15
-k_I = 5
 
 class Robot():
-    def __init__(self, mSx, mDx, cSx, cDx, controller, f, SPEED):
+    def __init__(self, mSx, mDx, cSx, cDx, controller, f, SPEED, MIN_SPEED, LOW_SPEED):
+        self.LOW_SPEED = LOW_SPEED
+        self.MIN_SPEED = MIN_SPEED
         self.SPEED = SPEED
         self.controller = controller   #allows to use a generic kind of controller, instead of forcing the use of Controller class
         self.f = f
@@ -25,29 +25,63 @@ class Robot():
     def readColor(self):
         return (COLORS[self.colorSx.value()], COLORS[self.colorDx.value()])
 
+    def normalize(self, speed):
+        if speed > 500:
+            return 500
+        if -self.MIN_SPEED < speed and speed < self.MIN_SPEED:
+            return -self.MIN_SPEED
+        if speed < -500:
+            return -500
+        return speed
+
+    def attach(self):
+        e = 0
+        color = ('red', 'red')
+        while color != ('yellow', 'yellow'):
+            if color == ('red', 'red'):
+                e = 0
+            if color[0] not in ['black','red'] or color[1] == 'black':
+                e = 1
+            if color[1] not in ['black','red'] or color[0] == 'black':
+                e = -1
+            corr = self.controller.getCorrection(e, self.LOW_SPEED, 'ATTACH')
+            self.move(self.normalize(self.LOW_SPEED - corr), self.normalize(self.LOW_SPEED + corr))
+            sleep(1/self.f)
+            color = self.readColor()
+
+
     def drive(self):
         e = 0
+        stop=0
         while True:
             color = self.readColor()
-            if color[0] == 'black':
-                if e > 0:
-                    e = 0
-                e -= 1
-            if color[1] == 'black':
-                if e < 0:
-                    e = 0
-                e += 1
-            if color == ('black', 'black'):
+            if color == ('red', 'red') :
+                self.attach()
                 self.stop()
                 sleep(5)
-            corr = self.controller.getCorrection(e, self.SPEED)
-            self.move(self.SPEED - corr, self.SPEED + corr)
+            if color == ('black', 'black'):
+                pass
+            else:
+                if color[0] == 'black':
+                    if e > 0:
+                        e = 0
+                    e -= 1
+                if color[1] == 'black':
+                    if e < 0:
+                        e = 0
+                    e += 1
+
+            corr = self.controller.getCorrection(e, self.SPEED, 'STD')
+            self.move(self.normalize(self.SPEED - corr), self.normalize(self.SPEED + corr))
             sleep(1/self.f)
 
+    def catch(self):
+        self.move(100, 100)
+        self.stop()
 
 class Controller():
-    def __init__(self, k_P, k_I):
-        self.k_P, self.k_I = k_P, k_I
+    def __init__(self, k_P, k_I, k_P_attach):
+        self.k_P, self.k_I, self.k_P_attach = k_P, k_I, k_P_attach
 
     def sgn(self, x):
         if x < 0:
@@ -56,10 +90,30 @@ class Controller():
             return 0
         return 1
 
-    def getCorrection(self, error, SPEED):  #error has integer values: negative if black is detected on the left and positive if it is detected on the right. The absolute value is the number of consecutive identical mesurements 
-        P = -k_P * self.sgn(error) * SPEED
-        I = -k_I * error * SPEED
-        return P + I
+    def getCorrection(self, error, SPEED, STATE):  #error has integer values: negative if black is detected on the left and positive if it is detected on the right. The absolute value is the number of consecutive identical mesurements
+        if STATE == 'STD':
+            P = -self.k_P * self.sgn(error) * SPEED/30
+            I = -self.k_I * error * SPEED/30
+            return P + I
+        if STATE == 'ATTACH':
+            P = -self.k_P_attach * self.sgn(error) * SPEED/30
+            return P
+
+class Nipper():
+    def __init__(self, motorMove, motorRotate, sensor):
+        self.motorMove, self.motorRotate, self.sensor = MediumMotor(motorMove), MediumMotor(motorRotate), ColorSensor(sensor)
+        self.sensor.mode = 'COL-COLOR'
+
+    def pull(self, colors):  # colors is a list of product colors which we are allowed to take
+        pass
+        #codice
+
+    def push(self):
+        pass
+        #codice
+
 
 if __name__=='main':
     Robot('outB', 'outA', 'in1', 'in2', Controller(15, 5), 10, 200).drive()
+
+
--- src/test.py
+++ src/test.py
@@ -3,10 +3,13 @@ from ev3dev.ev3 import *
 from time import sleep
 from robot import Robot, Controller
 
-STD_DIR_SPEED=200
+STD_DIR_SPEED=300
+MIN_SPEED=100
+LOW_SPEED=150
 
-r = Robot('outB', 'outA', 'in1', 'in2', Controller(15, 5), 10, STD_DIR_SPEED) # B->sinistro e A->destro
+r = Robot('outB', 'outA', 'in1', 'in2', Controller(10, 6, 10), 15, STD_DIR_SPEED, MIN_SPEED, LOW_SPEED) # B->sinistro e A->destro
 
 sleep(5)
 
 r.drive()
+#r.catch()
